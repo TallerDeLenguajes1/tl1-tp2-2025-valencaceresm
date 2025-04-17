98317372508895e52e8489ec1c69c45b9a5d148f
valencaceresm <caceresmendoza.valentina@gmail.com>
2025-04-17 15:51:04 -0300

    Agrego generación aleatoria de datos para 5 PCs

--- tp2_4.c
+++ tp2_4.c
@@ -11,3 +11,20 @@ struct compu {
 
 // arreglo predefinido de tipos de CPU
 char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
+
+int main(){
+    srand(time(NULL)); //inicializar la semilla para números aleatorios
+
+    struct compu pcs[5];
+
+    //generación aleatoria de datos para las 5 PCs
+    for (int i = 0; i < 5; i++){
+        pcs[i].velocidad = rand() % 3 + 1; //1 a 3 GHz
+        pcs[i].anio = rand() % 10 + 2015; //2015 a 2024
+        pcs[i].cantidad_nucleos = rand() % 8 + 1; //1 a 8 núcleos
+        pcs[i].tipo_cpu = tipos[rand() % 6]; //selección aleatoria de tipo CPU
+    }
+    
+    
+    return 0;
+}
